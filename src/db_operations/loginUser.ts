e8ebfad5c501028192d041893cc75b332e3281eb
David Piper <dbpiper@gmail.com>
2024-03-26 11:07:26 -0500

    Cleanup password check

--- src/db_operations/loginUser.ts
+++ src/db_operations/loginUser.ts
@@ -1,5 +1,4 @@
 import bcrypt from "bcrypt";
-import { Like } from "typeorm";
 import { AppDataSource } from "../db_connection/data-source";
 import { User } from "../db_models/User";
 
@@ -10,19 +9,11 @@ export async function loginUser(email: string, password: string) {
     console.log("error:", e);
   }
 
-  const salt = await bcrypt.genSalt();
-  console.log("salt:", salt);
-  console.log("salt.length:", salt.length);
-
-  // const user = new User(email, hashedPassword);
-
   const foundUser = await AppDataSource.manager.findOne(User, {
     where: { email },
   });
-  const foundUserSalt = foundUser?.hashedPassword.slice(0, 29) ?? "";
-  const generatedHashedPassword = await bcrypt.hash(password, foundUserSalt);
 
-  if (foundUser?.hashedPassword === generatedHashedPassword) {
+  if (await bcrypt.compare(password, foundUser?.hashedPassword ?? "")) {
     return foundUser;
   } else {
     return null;
