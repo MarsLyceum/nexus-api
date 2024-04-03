cf1701f7e9d5eccfecb51e957060ec6bd7c03279
David Piper <dbpiper@gmail.com>
2024-04-03 15:13:12 -0500

    Connect to Cloud DB

--- src/db_connection/createAppDataSource.ts
+++ src/db_connection/createAppDataSource.ts
@@ -2,16 +2,17 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from '../db_models/User';
 import { decryptDbPassword } from './decryptPassword';
-import { isRunningInDocker } from './isRunningInDocker';
+// import { isRunningInDocker } from './isRunningInDocker';
 
 export function createAppDataSource(): DataSource {
-    const DB_HOST = isRunningInDocker() ? 'host.docker.internal' : 'localhost';
+    // const DB_HOST = isRunningInDocker() ? 'host.docker.internal' : 'localhost';
+    const DB_HOST = '/cloudsql/hephaestus-418809:us-east1:hephaestus-postgres';
 
     return new DataSource({
         type: 'postgres',
         host: DB_HOST,
         port: 5433,
-        username: 'postgres',
+        username: 'hephaestus-db',
         password: decryptDbPassword(),
         database: 'postgres',
         synchronize: true,
