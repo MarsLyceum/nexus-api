f30a123425edef5eedfa758e4e3305445210d7b3
David Piper <dbpiper@gmail.com>
2024-04-18 13:20:46 -0500

    Fix cloud db key issue

--- src/db_connection/createAppDataSource.ts
+++ src/db_connection/createAppDataSource.ts
@@ -9,26 +9,29 @@ import { isRunningInDocker } from './isRunningInDocker';
 export function createAppDataSource(): DataSource {
     const cloudDb = isRunningInDocker();
     const sqlProxy = false;
-    const localDbSettings = sqlProxy
-        ? {
-              host: 'localhost',
-              PORT: 5432,
-              database: 'hephaestus-postgres',
-              username: 'hephaestus-db',
-              password: encode(
-                  Buffer.from('bDBJeXJpQ2BHaEtTeSIvMQ==', 'base64').toString(
-                      'utf8'
+    const localDbSettings =
+        !cloudDb &&
+        (sqlProxy
+            ? {
+                  host: 'localhost',
+                  PORT: 5432,
+                  database: 'hephaestus-postgres',
+                  username: 'hephaestus-db',
+                  password: encode(
+                      Buffer.from(
+                          'bDBJeXJpQ2BHaEtTeSIvMQ==',
+                          'base64'
+                      ).toString('utf8'),
+                      { mode: 'nonAsciiPrintable' }
                   ),
-                  { mode: 'nonAsciiPrintable' }
-              ),
-          }
-        : {
-              host: 'localhost',
-              PORT: 5432,
-              database: 'postgres',
-              username: 'postgres',
-              password: decryptDbPassword(),
-          };
+              }
+            : {
+                  host: 'localhost',
+                  PORT: 5432,
+                  database: 'postgres',
+                  username: 'postgres',
+                  password: decryptDbPassword(),
+              });
     const hostSettings = cloudDb
         ? {
               host: '/cloudsql/hephaestus-418809:us-west1:hephaestus-postgres',
