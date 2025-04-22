f551217c2321437f8b958e820e15c697591477b8
David Piper <dbpiper@gmail.com>
2025-04-22 13:18:03 -0500

    Make some build fixes

--- scripts/test-build.py
+++ scripts/test-build.py
@@ -1,6 +1,17 @@
 import subprocess
 import time
 import os
+import sys
+
+if len(sys.argv) > 1:
+    service_name = sys.argv[1]
+else:
+    service_name = os.path.basename(os.getcwd())
+
+# resolve the path to the repo root and its Dockerfile
+script_dir = os.path.dirname(os.path.abspath(__file__))
+repo_root = os.path.abspath(os.path.join(script_dir, os.pardir))
+dockerfile_path = os.path.join(repo_root, "Dockerfile")
 
 
 def run_command(command):
@@ -31,7 +42,7 @@ def highlight_logs(logs):
 # Build the Docker image
 print("Building Docker image...")
 build_status, build_output, build_error = run_command(
-    f"docker build -f {os.getcwd()}/Dockerfile -t local-test ../.."
+    f"docker build -f {dockerfile_path} -t local-test-{service_name} --build-arg SERVICE_NAME={service_name} ../.."
 )
 print(build_output)
 if build_status != 0:
@@ -41,7 +52,7 @@ if build_status != 0:
 # Remove any existing container with the same name
 print("Removing any existing container with the same name...")
 remove_status, remove_output, remove_error = run_command(
-    "docker rm -f local-test-container"
+    f"docker rm -f local-test-container-{service_name}"
 )
 if remove_status != 0:
     print(f"Warning: Could not remove existing container: {remove_error}")
@@ -49,7 +60,10 @@ if remove_status != 0:
 # Run the Docker container in detached mode
 print("Running Docker container in detached mode...")
 run_status, run_output, run_error = run_command(
-    "docker run --name local-test-container -d -p 4000:4000 local-test"
+    f"docker run --name local-test-container-{service_name} "
+    f"--env-file {repo_root}/services/{service_name}/.env "
+    f"-v {repo_root}/service-account-keys:/service-account-keys "
+    f" -d local-test-{service_name} "
 )
 if run_status != 0:
     print("Failed to start the container")
@@ -62,19 +76,19 @@ time.sleep(5)
 # Attach to the container logs
 print("Attaching to container logs...")
 log_status, log_output, log_error = run_command(
-    "docker logs local-test-container 2>&1"
+    f"docker logs local-test-container-{service_name} 2>&1"
 )
 
 # Check if the container is still running
 print("Checking if the container is still running...")
 ps_status, ps_output, ps_error = run_command(
-    "docker ps | grep local-test-container"
+    f"docker ps | grep local-test-container-{service_name}"
 )
 
 if ps_status == 0:
     print("Container started successfully")
     # Stop the container
-    run_command("docker stop local-test-container")
+    run_command(f"docker stop local-test-container-{service_name}")
     # Display the logs
     highlight_logs(log_output)
 else:
