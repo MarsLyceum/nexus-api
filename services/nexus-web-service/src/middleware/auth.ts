465eece8e5efcdeb4a1de21298911e44cdba4afe
David Piper <dbpiper@gmail.com>
2025-05-19 09:58:22 -0500

    Give more specific error messages for jwt auth

--- services/nexus-web-service/src/middleware/auth.ts
+++ services/nexus-web-service/src/middleware/auth.ts
@@ -21,11 +21,25 @@ export function authGuard({ operationName, req }: AuthGuardOptions): void {
     if (operationName && OPEN_OPERATIONS.has(operationName)) return;
 
     const token = getAccessToken(req);
-    if (!token) throw new UnauthenticatedError('Not authenticated');
+    if (!token) {
+        throw new UnauthenticatedError('Not authenticated. No token provided');
+    }
 
     try {
         jwt.verify(token, ACCESS_JWT_SECRET as jwt.Secret);
-    } catch {
-        throw new UnauthenticatedError('Invalid or expired token');
+    } catch (error: unknown) {
+        if (error instanceof jwt.TokenExpiredError) {
+            throw new UnauthenticatedError(
+                `Token expired at ${error.expiredAt.toISOString()}`
+            );
+        }
+
+        if (error instanceof jwt.JsonWebTokenError) {
+            throw new UnauthenticatedError(`Invalid token: ${error.message}`);
+        }
+
+        throw new UnauthenticatedError(
+            'Authentication failed due to an unexpected error'
+        );
     }
 }
